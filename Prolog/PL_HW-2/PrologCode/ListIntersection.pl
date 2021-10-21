e527fc1923abdb0ebc91fdd5bae3569113216f48
Nilansh <nil.netan963@gmail.com>
2021-10-21 19:09:03 -0400

    .

--- Prolog/PL_HW-2/PrologCode/ListIntersection.pl
+++ Prolog/PL_HW-2/PrologCode/ListIntersection.pl
@@ -1,4 +1,4 @@
-% Write a program that succeeds if the intersection 
+ Write a program that succeeds if the intersection 
 % of two given list parameters is empty
 
 % intersection : when there are same elements in lists
@@ -11,14 +11,14 @@ intersectionReturn ( [], [], []).
 intersectionReturn ( [], _ , []).
 intersectionReturn ( _, [], []).
 intersectionReturn ( [H|T], Y, Z) :-
-    
-    
 
+    
+% to return the joined lists together as one list
 joinlists ([], L, L).
 joinlists ([H|T], L1, [H|NewTail]) :-
     joinlists (T, L1, NewTail).
 
-
+% to check if an element is repeating or not 
 isRepeating (X, [X|_], [X]).
 isRepeating ( X, [_|Ta], []) :-
     isRepeating (X, T, [])
