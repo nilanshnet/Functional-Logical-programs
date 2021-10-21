2d8058d85727a618ec7a9277e013b457ab07e365
Nilansh <nil.netan963@gmail.com>
2021-10-20 22:14:34 -0400

    max number in a list

---
+++ Prolog/PL_HW-2/PrologCode/MaxofList.pl
@@ -0,0 +1,12 @@
+% program to find the max value of the
+%  elements of the given list
+
+% fact 1, the terminal case 
+% X is the max element of list with element X
+maxoflist([X],X).
+
+maxoflist([Start|EndTail],X) :-
+    maxoflist(EndTail,Y), (Start > Y -> X=Start; Y=X).
+   
+   
+   
