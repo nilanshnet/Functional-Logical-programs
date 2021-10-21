e61aeea571cc96b9a73b1b2bebd8183342e35165
Nilansh <nil.netan963@gmail.com>
2021-10-21 19:08:16 -0400

    update1

--- Prolog/PL_HW-2/PrologCode/ListIntersection.pl
+++ Prolog/PL_HW-2/PrologCode/ListIntersection.pl
@@ -3,3 +3,22 @@
 
 % intersection : when there are same elements in lists
 
+% Intersection ([1,2,3] , [3,4,5,6]) = False
+% Intersection ([], []) = [] , no intersection so True
+% Intersection ([1], [2,3,4] ) = True 
+
+intersectionReturn ( [], [], []).
+intersectionReturn ( [], _ , []).
+intersectionReturn ( _, [], []).
+intersectionReturn ( [H|T], Y, Z) :-
+    
+    
+
+joinlists ([], L, L).
+joinlists ([H|T], L1, [H|NewTail]) :-
+    joinlists (T, L1, NewTail).
+
+
+isRepeating (X, [X|_], [X]).
+isRepeating ( X, [_|Ta], []) :-
+    isRepeating (X, T, [])
