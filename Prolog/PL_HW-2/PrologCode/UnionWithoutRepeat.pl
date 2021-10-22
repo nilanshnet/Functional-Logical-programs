801ec76fc844cbf8497aa036a23cfa455afb2976
Nilansh <nil.netan963@gmail.com>
2021-10-22 17:32:06 -0400

    code added

--- Prolog/PL_HW-2/PrologCode/UnionWithoutRepeat.pl
+++ Prolog/PL_HW-2/PrologCode/UnionWithoutRepeat.pl
@@ -1,5 +1,19 @@
 % Write a program that returns a list containing the union 
 % of the elements of two given lists. 
 % Assume the list represents sets (i.e. unique elements) 
-% and the union returns unique elements.
+% and the union returns unique elements too.
+
+% when one list element is not repeating in the other list 
+joinlists([], X, X).
+joinlists([H|Tail], X, [H|Y]):-
+    joinlists(Tail, X, Y) -> \+ ifmember(H, X).
+
+% in case when element from List1 is a member of List2 as well
+joinlists([H|Tail], X, List):-
+    joinlists(Tail, X, List) ->  ifmember(H,X).
+
+% check if element is in the list
+ifmember(X,[X|_]).
+ifmember(X,[H|T]):-
+    ifmember(X,T) -> H=\=X.
 
