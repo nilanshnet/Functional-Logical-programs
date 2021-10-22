ca743541d2a41a7362686c24fc2b3372c92ee024
Nilansh <nil.netan963@gmail.com>
2021-10-22 01:10:40 -0400

    initialisation

---
+++ Prolog/PL_HW-2/PrologCode/UnionWithoutRepeat.pl
@@ -0,0 +1,5 @@
+% Write a program that returns a list containing the union 
+% of the elements of two given lists. 
+% Assume the list represents sets (i.e. unique elements) 
+% and the union returns unique elements.
+
