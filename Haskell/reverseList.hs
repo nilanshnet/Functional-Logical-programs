8c9d36059b01e0795ed870d301256994b44bb51f
Nilansh <nil.netan963@gmail.com>
2021-10-27 01:09:01 -0400

    fix

--- Haskell/reverseList.hs
+++ Haskell/reverseList.hs
@@ -1,6 +1,17 @@
 -- reverse the list using tail recurssion
 
+rev :: [a] -> [a]
+rev list = revHelper list []
+
 -- helper function that appends into a blank list
-revHelper [a] -> [a] -> [a]
+revHelper :: [a] -> [a] -> [a]
 revHelper [] a = a
 revHelper (x:xs) a = revHelper xs (x:a)
+
+-- main 
+main = do {
+        print(rev [1,2,3,4]);
+    }
+
+
+
